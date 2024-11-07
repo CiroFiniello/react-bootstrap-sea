919adc397db8549ff528d9137cb88d18637e5f36
CiroFiniello <kirofiniello@outlook.it>
2024-11-07 23:04:52 +0100

    ho creato una sezione commenti, aggiunto modifiche di transizione e di hover ai pulsanti, e agli input, e ho iniziato a generare la sezione commenti, ho testato l'immagine del commento dalla libreria in via temporanea, passo alla realizzazione della parte dinamica di questa sezione

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,6 +1,43 @@
 import React from "react";
+import "./comments.scss";
+import csgo from "../../assets/csgo.webp";
 
 const Comments = () => {
-  return <h1>Comments</h1>;
+  return (
+    <div className="comments pt-2">
+      <section className="title row mt-3">
+        <div className="box col-md-6 mt-3">
+          <h6>Commenti</h6>
+          <p>Mostra tutti i commenti</p>
+        </div>
+        <div className="box col-md-6 align-content-center gap-1">
+          <input type="checkbox" />
+          <span className="ms-2">segui discussione</span>
+        </div>
+      </section>
+      <section className="input row mt-3 mx-1 p-3">
+        <input
+          type="text"
+          className="commenta col-md-9 pt-3 me-3"
+          placeholder="Aggiungi un commento"
+        />
+        <button className="btn-main col-md-2">Pubblica</button>
+
+        <section className="data">
+          <div className="comment row pt-3">
+            <figure className="col-md-2 immagine">
+              <img src={csgo} alt="img" className="avatar" />
+            </figure>
+            <article className="col-md-9">
+              <h6>Friend</h6>
+              <div className="commento">
+                <p>questo è il commento</p>
+              </div>
+            </article>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
 };
 export default Comments;
